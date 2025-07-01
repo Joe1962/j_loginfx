78bc7c2572b1132fc8470d3d7638c04956d85684
joe1962 <joe1962@gmail.com>
2025-07-01 17:19:15 -0400

    Fixed copyrights. Added LibInfo.

---
+++ src/main/java/cu/jsoft/j_loginfx/LibInfo.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright Joe1962
+ * https://github.com/Joe1962
+ */
+package cu.jsoft.j_loginfx;
+
+/**
+ *
+ * @author joe1962
+ */
+public class LibInfo {
+
+	// Lib constants:
+	private static final String TITLE = "jloginfx";
+	private static final String VERSION = "1.0.0";
+	private static final String BUILD = "250701.01";
+
+	/**
+	 * @return the TITLE
+	 */
+	public static String getTITLE() {
+		return TITLE;
+	}
+
+	/**
+	 * @return the VERSION
+	 */
+	public static String getVERSION() {
+		return VERSION;
+	}
+
+	/**
+	 * @return the BUILD
+	 */
+	public static String getBUILD() {
+		return BUILD;
+	}
+
+	public static LibInfo getInstance() {
+		return infoHolder.INSTANCE;
+	}
+
+	private LibInfo() {
+	}
+
+	private static class infoHolder {
+
+		private static final LibInfo INSTANCE = new LibInfo();
+	}
+
+}
